4c51a37119c0e8fd645420d1e35061b92faf7abc
lupetalo <lupetalo@gmail.com>
2023-03-22 16:17:54 +0100

    flask-reuoloaded

--- filedrop.py
+++ filedrop.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask_uploads import UploadSet, configure_uploads, ALL
+from flask_reuoloaded import UploadSet, configure_uploads, ALL
 
 fileset = UploadSet('files', ALL)
 
